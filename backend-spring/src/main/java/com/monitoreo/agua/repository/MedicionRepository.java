33b582be89bb3d33e3a1a0c52e5bf8fe175f5ebb
Bayronfc05 <158859490+Bayronfc05@users.noreply.github.com>
2025-11-16 10:53:55 -0500

    Revert "feat: Implementación completa del backend Spring Boot"

--- backend-spring/src/main/java/com/monitoreo/agua/repository/MedicionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monitoreo.agua.repository;
-
-import com.monitoreo.agua.entity.Medicion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicionRepository extends JpaRepository<Medicion, Long> {
-}
